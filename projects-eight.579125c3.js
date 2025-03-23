8a8a0ab20a851595a8aa76416796268df39841ac
Руслан Івасюк <126970988+ZxcMimis@users.noreply.github.com>
2025-03-23 17:00:13 +0200

    Updates

---
+++ projects-eight.579125c3.js
@@ -0,0 +1,47 @@
+document.getElementById('contactForm').addEventListener('submit', addContact);
+function loadContacts() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    const contactList = document.getElementById('contactList');
+    contactList.innerHTML = '';
+    contacts.forEach((contact, index)=>{
+        const li = document.createElement('li');
+        li.innerHTML = `${contact.firstName} ${contact.lastName} - ${contact.phone} - ${contact.email}
+            <button class="edit-button" onclick="editContact(${index})">\u{420}\u{435}\u{434}\u{430}\u{433}\u{443}\u{432}\u{430}\u{442}\u{438}</button>
+            <button class="delete-button" onclick="deleteContact(${index})">\u{412}\u{438}\u{434}\u{430}\u{43B}\u{438}\u{442}\u{438}</button>`;
+        contactList.appendChild(li);
+    });
+}
+function addContact(event) {
+    event.preventDefault();
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const phone = document.getElementById('phone').value;
+    const email = document.getElementById('email').value;
+    const contacts = JSON.parse(localStorage.getItem('contacts')) || [];
+    contacts.push({
+        firstName,
+        lastName,
+        phone,
+        email
+    });
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+    document.getElementById('contactForm').reset();
+    loadContacts();
+}
+function deleteContact(index) {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    contacts.splice(index, 1);
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+    loadContacts();
+}
+function editContact(index) {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    const contact = contacts[index];
+    document.getElementById('firstName').value = contact.firstName;
+    document.getElementById('lastName').value = contact.lastName;
+    document.getElementById('phone').value = contact.phone;
+    document.getElementById('email').value = contact.email;
+    editIndex = index;
+}
+
+//# sourceMappingURL=projects-eight.579125c3.js.map
